b968ca098dafc4bbf97e891affccc33b96abd587
GTECH\mykhaylo.k <mykhaylo.k@sbtech.com>
2018-03-27 13:19:04 +0300

    t

--- AspNetCore2App/Program.cs
+++ AspNetCore2App/Program.cs
@@ -17,6 +17,7 @@ namespace AspNetCore2App
             BuildWebHost(args).Run();
         }
 
+
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
